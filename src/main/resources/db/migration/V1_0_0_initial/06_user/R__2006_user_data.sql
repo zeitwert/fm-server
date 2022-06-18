4cd5d0aeb37b13ae0b4b9666dbe63fce73b27f85
Hannes Brunner <hannes_brunner@hotmail.com>
2022-06-18 14:37:47 +0200

    Admin pwd

--- src/main/resources/db/migration/V1_0_0_initial/06_user/R__2006_user_data.sql
+++ src/main/resources/db/migration/V1_0_0_initial/06_user/R__2006_user_data.sql
@@ -10,9 +10,9 @@ insert into migr_obj_user_v(tenant,name,email,password,role_list,picture) values
 ('comunas', 'Martin Frey', 'martin@comunas.ch', '{noop}comunas', 'ADMIN,USER', '/demo/comunas/martin.jpg'),
 ('comunas', 'Xavier Frey', 'xavier@comunas.ch', '{noop}comunas', 'USER', '/demo/comunas/xavier.jpg'),
 ('comunas', 'Hannes Brunner', 'hannes@comunas.ch', '{noop}comunas', 'ADMIN,USER', 'https://randomuser.me/api/portraits/lego/4.jpg'),
-('unteraegeri', 'Admin', 'admin@unteraegeri.ch', '{noop}comunas', 'ADMIN', 'https://randomuser.me/api/portraits/lego/0.jpg'),
+('unteraegeri', 'Admin', 'admin@unteraegeri.ch', '{noop}admin', 'ADMIN', 'https://randomuser.me/api/portraits/lego/0.jpg'),
 ('unteraegeri', 'Martin Frey', 'martin.frey@unteraegeri.ch', '{noop}comunas', 'USER', '/demo/comunas/martin.jpg'),
 ('unteraegeri', 'Gregor Inderwildi', 'gregor.inderwildi@unteraegeri.ch', '{noop}comunas', 'USER', '/demo/unteraegeri/gregor.png'),
-('staefa', 'Admin', 'admin@staefa.ch', '{noop}comunas', 'ADMIN', 'https://randomuser.me/api/portraits/lego/0.jpg'),
+('staefa', 'Admin', 'admin@staefa.ch', '{noop}admin', 'ADMIN', 'https://randomuser.me/api/portraits/lego/0.jpg'),
 ('staefa', 'Martin Frey', 'martin.frey@staefa.ch', '{noop}comunas', 'USER', '/demo/comunas/martin.jpg'),
 ('staefa', 'Marlies Morger', 'marlies.morger@staefa.ch', '{noop}comunas', 'USER', null);
