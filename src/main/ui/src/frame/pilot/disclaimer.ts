04552e88776e5e7171c87078f429ef45ef357d59
hannes_brunner@hotmail.com <hannes_brunner@hotmail.com>
2022-07-22 03:19:06 +0200

    Improve disclaimer for pilot

--- src/main/ui/src/frame/pilot/disclaimer.ts
+++ src/main/ui/src/frame/pilot/disclaimer.ts
@@ -1,8 +1,10 @@
 
 const text =
-	"Mit dem Login in comunas.fm akzeptiere ich die folgenden Bedingungen/Einschränkungen für den Pilotbetrieb:\n\n" +
+	"Mit dem Login in comunas.fm akzeptiere ich die folgenden Bedingungen bzw Einschränkungen für den Pilotbetrieb:\n\n" +
 	"**comunas GmbH** und/oder **zeitwert GmbH** übernehmen **keine Verantwortung** für\n\n" +
-	"1. einen Datenverlust (wir werden jedoch periodisch (wöchentlich) die Daten manuell sichern, so dass sich ein allfälliger Datenverlust auf maximal eine Woche beschränken sollte)\n" +
-	"2. Fehler in den Berechnungen. Wir empfehlen die Berechnungen zuerst durch comunas GmbH überprüfen zu lassen, bevor sie als Grundlage für eine Budgetierung herangezogen werden.\n";
+	"1. **einen Datenverlust** (wir werden jedoch die Daten periodisch manuell sichern, so dass ein allfälliger Datenverlust beschränkt sein wird)\n" +
+	"2. **Fehler in den Bewertungen**. Wir empfehlen die Bewertungen zuerst durch comunas GmbH überprüfen zu lassen, bevor sie als Grundlage für eine Budgetierung herangezogen werden.\n\n" +
+	"**best effort SLA**\n" +
+	"Allfällige Fehler in der Software oder Probleme mit deren Bedienung werden nach bestem Bemühen behoben, es gibt aber keine verbindliche Zusagen für Antwortzeiten\n";
 
 export default text;
