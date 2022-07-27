75a31868014ebb4995ed87589d7e0484c8d49cf5
Hannes Brunner <hannes_brunner@hotmail.com>
2022-07-27 19:52:53 +0200

    Reverse rating barometer

--- src/main/ui/src/building/ui/forms/ElementRatingForm.tsx
+++ src/main/ui/src/building/ui/forms/ElementRatingForm.tsx
@@ -80,39 +80,39 @@ export default class ElementRatingForm extends React.Component<ElementRatingForm
 													<div className="h5">Jahre</div>
 												</div>
 												<div className="d1">
-													<div className="d11 slds-align_absolute-center">Schadhaft</div>
-													<div className="d12 slds-align_absolute-center">Stark</div>
-													<div className="d13 slds-align_absolute-center">Mittel</div>
-													<div className="d14 slds-align_absolute-center">Leicht</div>
+													<div className="d1h1">Intakt</div>
+													<div className="d1t1"><div className="txt"><span>Neu</span></div></div>
+													<div className="d1t2"><div className="txt"><span>Gebraucht</span></div></div>
 												</div>
 												<div className="d2">
-													<div className="d21">Intakt</div>
-													<div className="d22"><div className="d22t"><span>Gebraucht</span></div></div>
-													<div className="d23"><div className="d23t"><span>Neu</span></div></div>
+													<div className="d2h1 slds-align_absolute-center">Schadhaft</div>
+													<div className="d2t1 slds-align_absolute-center">Leicht</div>
+													<div className="d2t2 slds-align_absolute-center">Mittel</div>
+													<div className="d2t3 slds-align_absolute-center">Stark</div>
 												</div>
 												<div className="d3">
-													<div className="d31"></div>
-													<div className="d32"></div>
-													<div className="d33"></div>
-													<div className="d34"></div>
+													<div className="d3p1"></div>
+													<div className="d3p2"></div>
+													<div className="d3p3"></div>
+													<div className="d3p4"></div>
 													<div className={"zn " + (element.condition ? "zn-" + (5 * Math.round(element.condition! / 5)) : "")}>
 													</div>
 												</div>
 												<div className="d4">
-													<div className="t20 slds-align_absolute-center">20</div>
-													<div className="t50 slds-align_absolute-center">50</div>
-													<div className="t70 slds-align_absolute-center">70</div>
-													<div className="t85 slds-align_absolute-center">85</div>
-													<div className="t95 slds-align_absolute-center">95</div>
 													<div className="t100 slds-align_absolute-center">100</div>
+													<div className="t95 slds-align_absolute-center">95</div>
+													<div className="t85 slds-align_absolute-center">85</div>
+													<div className="t70 slds-align_absolute-center">70</div>
+													<div className="t50 slds-align_absolute-center">50</div>
+													<div className="t20 slds-align_absolute-center">20</div>
 												</div>
 												<div className="d5">
-													<div className="t20 slds-align_absolute-center">{element.lifeTime20}</div>
-													<div className="t50 slds-align_absolute-center">{element.lifeTime50}</div>
-													<div className="t70 slds-align_absolute-center">{element.lifeTime70}</div>
-													<div className="t85 slds-align_absolute-center">{element.lifeTime85}</div>
-													<div className="t95 slds-align_absolute-center">{element.lifeTime95}</div>
 													<div className="t100 slds-align_absolute-center">{element.lifeTime100}</div>
+													<div className="t95 slds-align_absolute-center">{element.lifeTime95}</div>
+													<div className="t85 slds-align_absolute-center">{element.lifeTime85}</div>
+													<div className="t70 slds-align_absolute-center">{element.lifeTime70}</div>
+													<div className="t50 slds-align_absolute-center">{element.lifeTime50}</div>
+													<div className="t20 slds-align_absolute-center">{element.lifeTime20}</div>
 												</div>
 											</div>
 										</div>
