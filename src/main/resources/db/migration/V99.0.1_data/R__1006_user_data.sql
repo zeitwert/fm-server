f920880fddae5c805a32c1699dbf933a9ce2a7a9
Hannes Brunner <hannes_brunner@hotmail.com>
2022-11-25 12:19:56 +0100

    Fix user migration for tenant access

--- src/main/resources/db/migration/V99.0.1_data/R__1006_user_data.sql
+++ src/main/resources/db/migration/V99.0.1_data/R__1006_user_data.sql
@@ -16,6 +16,60 @@ insert into migr_obj_user_v(tenant,name,email,password,role_list,picture) values
 ('staefa', 'Martin Frey', 'martin.frey@staefa.ch', '{noop}comunas', 'super_user', '/demo/comunas/martin.jpg'),
 ('staefa', 'Marlies Morger', 'marlies.morger@staefa.ch', '{noop}comunas', 'read_only', null);
 
+-- fix app_admin role
 update obj_user
 set    role_list = 'app_admin'
 where  email = 'k@zeitwert.io';
+
+-- fix tenant access
+insert	into obj_part_item(
+					obj_id,
+					parent_part_id,
+					part_list_type_id,
+					seq_nr,
+					item_id
+				)
+select	id,
+				0,
+				'user.tenantList',
+				0,
+				tenant_id
+from		obj_user_v;
+
+-- allow martin@comunas.ch access to clients
+insert	into obj_part_item(
+					obj_id,
+					parent_part_id,
+					part_list_type_id,
+					seq_nr,
+					item_id
+				)
+select	uv.id,
+				0,
+				'user.tenantList',
+				2,
+				tv.obj_id
+from		obj_user_v uv
+left join obj_tenant tv on tv.extl_key = 'unteraegeri'
+where		email = 'martin@comunas.ch';
+
+insert	into obj_part_item(
+					obj_id,
+					parent_part_id,
+					part_list_type_id,
+					seq_nr,
+					item_id
+				)
+select	uv.id,
+				0,
+				'user.tenantList',
+				3,
+				tv.obj_id
+from		obj_user_v uv
+left join obj_tenant tv on tv.extl_key = 'staefa'
+where		email = 'martin@comunas.ch';
+
+-- close martin@customer
+update	obj
+set			closed_at = now()
+where		id in (select obj_id from obj_user_v where email in ('martin.frey@unteraegeri.ch', 'martin.frey@staefa.ch'));
--- src/main/resources/db/test-data/V99.0.2_test_data/R__2006_user_data.sql
+++ src/main/resources/db/test-data/V99.0.2_test_data/R__2006_user_data.sql
@@ -8,3 +8,36 @@ insert into migr_obj_user_v(tenant,name,email,password,role_list,picture) values
 ('demo', 'Simon Schatzmann', 'simon@zeitwert.io', '{noop}demo', 'user', 'https://randomuser.me/api/portraits/men/3.jpg'),
 ('demo', 'Barbara Buchhalter', 'barbara@zeitwert.io', '{noop}demo', 'user', 'https://randomuser.me/api/portraits/women/3.jpg'),
 ('demo', 'Verena Verwalterin', 'verena@zeitwert.io', '{noop}demo', 'super_user', 'https://randomuser.me/api/portraits/women/2.jpg');
+
+-- fix tenant access
+insert	into obj_part_item(
+					obj_id,
+					parent_part_id,
+					part_list_type_id,
+					seq_nr,
+					item_id
+				)
+select	uv.id,
+				0,
+				'user.tenantList',
+				0,
+				uv.tenant_id
+from		obj_user_v uv
+where		not exists (select null from obj_part_item pi where pi.obj_id = uv.id and pi.part_list_type_id = 'user.tenantList');
+
+-- allow all comunas to access demo
+insert	into obj_part_item(
+					obj_id,
+					parent_part_id,
+					part_list_type_id,
+					seq_nr,
+					item_id
+				)
+select	id,
+				0,
+				'user.tenantList',
+				(select max(seq_nr)+1 from obj_part_item opi where opi.obj_id = uv.id and opi.part_list_type_id = 'user.tenantList'),
+				(select t.obj_id from obj_tenant t where t.extl_key = 'demo')
+from		obj_user_v uv
+where		email like '%@comunas.ch';
+
