ca5b8547cb3b55f0cafe9d797ddc9599e5a85152
Hannes Brunner <hannes_brunner@hotmail.com>
2023-01-26 10:25:53 +0100

    Simplify TextField

--- src/main/ui/src/@zeitwert/ui-forms/model/TextField.ts
+++ src/main/ui/src/@zeitwert/ui-forms/model/TextField.ts
@@ -1,33 +1,35 @@
 
-import { ConversionError, Field, FieldOptions, StringConverter } from "mstform";
+//import { ConversionError, Field, FieldOptions, StringConverter } from "mstform";
 
-type StringOptions = {
-	maxLength?: number;
-};
+import { converters, Field, FieldOptions } from "mstform";
 
-function string(options?: StringOptions) {
-	return new StringConverter<string>({
-		emptyRaw: "",
-		emptyValue: "",
-		convert(raw) {
-			if (options?.maxLength && options.maxLength < raw.length) {
-				throw new ConversionError("exceedsMaxLength");
-			}
-			return raw;
-		},
-		render(value) {
-			return value || this.emptyRaw;
-		},
-		preprocessRaw(raw: string): string {
-			return raw.trim();
-		},
-	});
-}
+// type StringOptions = {
+// 	maxLength?: number;
+// };
 
-export class TextField extends Field<string, string | undefined> {
+// function string(options?: StringOptions) {
+// 	return new StringConverter<string>({
+// 		emptyRaw: "",
+// 		emptyValue: "",
+// 		convert(raw) {
+// 			if (options?.maxLength && options.maxLength < raw.length) {
+// 				throw new ConversionError("exceedsMaxLength");
+// 			}
+// 			return raw;
+// 		},
+// 		render(value) {
+// 			return value || this.emptyRaw;
+// 		},
+// 		preprocessRaw(raw: string): string {
+// 			return raw.trim();
+// 		},
+// 	});
+// }
+
+export class TextField extends Field<string | null, string | undefined> {
 	constructor(
-		options?: FieldOptions<string, string | undefined, any, any>
+		options?: FieldOptions<string | null, string | undefined, any, any>
 	) {
-		super(string(), options);
+		super(converters.maybe(converters.string), options);
 	}
 }
