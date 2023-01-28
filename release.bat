cf35f228f4fb705e7852edcf48e9e92d242635df
hannes_brunner@hotmail.com <hannes_brunner@hotmail.com>
2023-01-28 17:29:49 +0100

    next version release script

--- release.bat
+++ release.bat
@@ -1,7 +1,20 @@
 
 @echo off
 
-mvn -B clean release:prepare -Dresume=false
+mvn -B -Darguments=-DskipTests clean release:prepare -Dresume=false
+
+git reset HEAD~1
+git stash
+git push heroku main
+git stash pop
+git add -u
+git commit -m "Deployed to heroku, prepare for next release"
+
+del pom.xml.releaseBackup
+
+exit
+
+
 
 for /f %%a in ('git describe --tags --abbrev^=0 main') do (
 	set LATEST_TAG=%%a
@@ -13,4 +26,3 @@ git push heroku %LATEST_TAG%:main
 git add -u
 git commit -m "released to heroku"
 
-del pom.xml.releaseBackup
