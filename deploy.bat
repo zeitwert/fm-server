4cd55177809b3c3a7f4b86eda611cf99ead1bf66
Hannes Brunner <hannes_brunner@hotmail.com>
2023-01-29 21:36:25 +0100

    release.bat => deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,15 @@
+
+@echo off
+
+call mvn -B -Darguments=-DskipTests clean release:prepare -Dresume=false -DpushChanges=false
+
+git reset HEAD~1
+git stash
+git push heroku main
+git stash pop
+git add -u
+git commit -m "Deployed to heroku, prepare for next release"
+
+del pom.xml.releaseBackup
+
+git push
