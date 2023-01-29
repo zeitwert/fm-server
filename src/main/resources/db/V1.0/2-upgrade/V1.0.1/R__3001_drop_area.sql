fd7486819b36c61a32aee489b146cefbc650cd18
Hannes Brunner <hannes_brunner@hotmail.com>
2023-01-29 23:45:51 +0100

    Drop area, part II

--- src/main/resources/db/V1.0/2-upgrade/V1.0.1/R__3001_drop_area.sql
+++ src/main/resources/db/V1.0/2-upgrade/V1.0.1/R__3001_drop_area.sql
@@ -1,4 +1,10 @@
 
+delete from obj_part_item
+where part_list_type_id = 'obj.areaSet';
+
+delete from doc_part_item
+where part_list_type_id = 'doc.areaSet';
+
 delete from code_part_list_type
 where id = 'doc.areaSet'
    or id = 'obj.areaSet';
