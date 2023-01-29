29b2e628bdcd64302808e1ce6d86bc2aefcad783
hannes_brunner@hotmail.com <hannes_brunner@hotmail.com>
2023-01-29 16:17:11 +0100

    Push after release

--- release.bat
+++ release.bat
@@ -12,17 +12,4 @@ git commit -m "Deployed to heroku, prepare for next release"
 
 del pom.xml.releaseBackup
 
-exit
-
-
-
-for /f %%a in ('git describe --tags --abbrev^=0 main') do (
-	set LATEST_TAG=%%a
-)
-
-echo Pushing %LATEST_TAG% to heroku
-git push heroku %LATEST_TAG%:main
-
-git add -u
-git commit -m "released to heroku"
-
+git push
