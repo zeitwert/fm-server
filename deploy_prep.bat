6ed46755d901b1f40a396f0a21f50a684927f32f
Hannes Brunner <hannes_brunner@hotmail.com>
2023-07-30 15:48:13 +0200

    update readme regarding deployment

--- deploy_prep.bat
+++ deploy_prep.bat
@@ -3,7 +3,7 @@
 
 git push
 
-mvn release:clean
+mvn clean release:clean
 
 rem don't push changes to remote repo, we'll do that manually later
 rem otherwise new snapshot version will already be pushed to heroku
