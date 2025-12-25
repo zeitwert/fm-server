76f3260a145fdbb8f7850f482dc2ab47a17b1e49
Hannes Brunner <hannes_brunner@hotmail.com>
2025-12-25 17:12:23 +0100

    property by delegate (viii)

--- fm-domain/src/main/java/io/zeitwert/fm/collaboration/model/impl/ObjNoteImpl.kt
+++ fm-domain/src/main/java/io/zeitwert/fm/collaboration/model/impl/ObjNoteImpl.kt
@@ -1,25 +1,24 @@
-package io.zeitwert.fm.collaboration.model.base
+package io.zeitwert.fm.collaboration.model.impl
 
 import io.dddrive.ddd.model.Aggregate
+import io.dddrive.property.delegate.baseProperty
+import io.dddrive.property.delegate.enumProperty
 import io.zeitwert.fm.collaboration.model.ObjNote
 import io.zeitwert.fm.collaboration.model.ObjNoteRepository
 import io.zeitwert.fm.collaboration.model.enums.CodeNoteType
 import io.zeitwert.fm.obj.model.base.FMObjBase
 
-abstract class ObjNoteBase(
+open class ObjNoteImpl(
 	override val repository: ObjNoteRepository,
 	isNew: Boolean,
 ) : FMObjBase(repository, isNew),
 	ObjNote {
 
-	override fun doInit() {
-		super.doInit()
-		addBaseProperty("relatedToId", Any::class.java)
-		addEnumProperty("noteType", CodeNoteType::class.java)
-		addBaseProperty("subject", String::class.java)
-		addBaseProperty("content", String::class.java)
-		addBaseProperty("isPrivate", Boolean::class.java)
-	}
+	override var relatedToId: Any? by baseProperty()
+	override var noteType: CodeNoteType? by enumProperty()
+	override var subject: String? by baseProperty()
+	override var content: String? by baseProperty()
+	override var isPrivate: Boolean? by baseProperty()
 
 	override fun doCalcAll() {
 		super.doCalcAll()
--- fm-domain/src/main/java/io/zeitwert/fm/collaboration/model/impl/ObjNoteRepositoryImpl.kt
+++ fm-domain/src/main/java/io/zeitwert/fm/collaboration/model/impl/ObjNoteRepositoryImpl.kt
@@ -2,21 +2,22 @@ package io.zeitwert.fm.collaboration.model.impl
 
 import io.zeitwert.fm.collaboration.model.ObjNote
 import io.zeitwert.fm.collaboration.model.ObjNoteRepository
-import io.zeitwert.fm.collaboration.model.base.ObjNoteBase
 import io.zeitwert.fm.obj.model.base.FMObjRepositoryBase
 import org.springframework.stereotype.Component
 
 @Component("objNoteRepository")
-class ObjNoteRepositoryImpl : FMObjRepositoryBase<ObjNote>(
-	ObjNoteRepository::class.java,
-	ObjNote::class.java,
-	ObjNoteBase::class.java,
-	AGGREGATE_TYPE_ID
-), ObjNoteRepository {
+class ObjNoteRepositoryImpl :
+	FMObjRepositoryBase<ObjNote>(
+		ObjNoteRepository::class.java,
+		ObjNote::class.java,
+		ObjNoteImpl::class.java,
+		AGGREGATE_TYPE_ID,
+	),
+	ObjNoteRepository {
 
 	companion object {
 
 		private const val AGGREGATE_TYPE_ID = "obj_note"
 	}
-}
 
+}
